92839412ef80f16bf4b88b9572664873d0a3f0c8
KEVIN RAMIREZ <kevin.alexanderra1302@gmail.com>
2025-11-02 22:31:32 +0000

    cors

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 import express from 'express';
 const app = express();
 import routes from './routes/routes.js';
+import cors from 'cors';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
 app.use(routes);
 
 app.listen(3000, () => {
